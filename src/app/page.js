c065ea10fac4db0243d4a96e8adcf49403537afc
Salman Farshi <devwithfarshi@gmail.com>
2024-09-14 10:46:02 +0600

    🆕:add Favicon added

--- src/app/page.js
+++ src/app/page.js
@@ -3,7 +3,7 @@ import Container from "@/components/common/Container";
 const Home = () => {
   return (
     <>
-      <Container>Let&apos; start</Container>
+      <Container>start</Container>
     </>
   );
 };
---
+++ src/components/shared/Header/index.jsx
@@ -0,0 +1,10 @@
+import Container from "@/components/common/Container";
+
+const Header = () => {
+  return (
+    <header>
+      <Container>Header</Container>
+    </header>
+  );
+};
+export default Header;
